d09e0289ea0c347084aacd6c8e136bc7048b7b8f
Dancing bull <72798082+upscaspirantmzr@users.noreply.github.com>
2025-08-08 17:59:28 +0530

    Create firebase_config.js

---
+++ firebase_config.js
@@ -0,0 +1,17 @@
+// This file contains your Firebase configuration.
+// You can get this from your Firebase project settings.
+// This is the only file you will need to edit to connect to your project.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_L2-UjpymkCwUwuQyjlUxXj7T93KE4KA",
+  authDomain: "constructionmzr.firebaseapp.com",
+  projectId: "constructionmzr",
+  storageBucket: "constructionmzr.firebasestorage.app",
+  messagingSenderId: "1012956741536",
+  appId: "1:1012956741536:web:1da6959b3d3330b82a6ec5",
+  measurementId: "G-D703VCW38R"
+};
+
+// Do not edit below this line.
+// The code in the other files will reference this `firebaseConfig` variable.
+export { firebaseConfig };
